fefc85520f514a27e1c9e913ee2817c0dd6caf56
AXCWG <xiey0@qq.com>
2024-11-14 02:19:23 +0800

    final fix for async func connect try catch

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
-const { readFileSync } = require("fs")
-const { createServer } = require("https")
+const {readFileSync} = require("fs")
+const {createServer} = require("https")
 const ws = require("ws")
 const net = require('net')
 const crypto = require('crypto')
 
-const server = createServer({
-    cert: readFileSync('./andyxie.cn.pem'),
-    key: readFileSync("./andyxie.cn.key")
-})
-const wss = new ws.WebSocketServer({server})
-// const wss = new ws.WebSocketServer({
-//     port: "8090"
+// const server = createServer({
+//     cert: readFileSync('./andyxie.cn.pem'),
+//     key: readFileSync("./andyxie.cn.key")
 // })
+// const wss = new ws.WebSocketServer({server})
+
+const wss = new ws.WebSocketServer({
+    port: "8090"
+})
 
 wss.on('connection', function (ws, req) {
     console.log("Connected from: " + req.socket.remoteAddress)
     var client = new net.Socket();
-
     ws.on("message", (data) => {
-        
-        try{
+
+        try {
             var preprocess = JSON.parse(data.toString())
 
-        }catch(ex){
+        } catch (ex) {
             ws.send(ex.toString())
         }
 
@@ -31,18 +31,20 @@ wss.on('connection', function (ws, req) {
                 ws.on("close", () => {
                     client.destroy()
                 })
-                try{
-                    client.connect(preprocess.host.split(":")[1], preprocess.host.split(":")[0], () => {
-                        client.on("data", (data) => {
-                            ws.send(data.toString())
-                        })
-    
-                        client.write("iwtcms_login " + crypto.createHash('sha256').update(preprocess.password).digest('hex') + "\n")
+
+                client.on("error", (err) => {
+                    ws.send(err.toString()+"\n")
+                })
+                client.connect(preprocess.host.split(":")[1], preprocess.host.split(":")[0], () => {
+                    client.on("data", (data) => {
+                        ws.send(data.toString())
                     })
-                }catch(ex){
-                    ws.send(JSON.stringify({status: 1, message: ex.toString()}))
-                }
-                
+
+                    client.write("iwtcms_login " + crypto.createHash('sha256').update(preprocess.password).digest('hex') + "\n")
+                })
+
+
+
             } catch (ex) {
                 ws.send("Login failed! " + ex.toString() + "\n")
             }
@@ -64,4 +66,4 @@ wss.on('close', (ws) => {
 })
 
 
-server.listen(8090)
+// server.listen(8090)
